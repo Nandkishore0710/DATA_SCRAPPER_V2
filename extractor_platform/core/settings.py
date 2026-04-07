b58c9e5252c99cab50d07c36162468b9e63d8ff9
Nand Kishor <nandkishore2530@gmail.com>
2026-04-07 15:12:22 +0530

    Whitelisted scraper.7shouters.com in Django settings for domain access.

--- extractor_platform/core/settings.py
+++ extractor_platform/core/settings.py
@@ -5,7 +5,9 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost,scraper.7shouters.com').split(',')
+ALLOWED_HOSTS.append('scraper.7shouters.com')
+ALLOWED_HOSTS.append('168.144.78.131')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
